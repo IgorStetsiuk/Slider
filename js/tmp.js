4732f2560531e43ebd473d5150986dcf6c332dbb
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-02-03 19:59:30 +0200

    half done

---
+++ js/tmp.js
@@ -0,0 +1,28 @@
+
+// let iterator = config.itemsPerPage;
+// this.slides.map(item =>{
+//    if(item.classList.contains('visible')) {
+//        item.classList.remove('visible');
+//        return item;
+//    }
+//  while (iterator!==0 && iterator-- ){
+//        return item.classList.add('visible')
+//  }
+//
+// });
+
+// const arrayToFilter = config.currentVisibleSlides || this.slides;
+//
+//
+// const nextBatch = arrayToFilter.filter(item => {
+//     if (item.classList.contains('visible')) {
+//         item.classList.remove('visible');
+//         config.currentVisibleSlides = arrayToFilter.splice(1,1);
+//
+//         return;
+//     }
+//     return item;
+//
+//
+// });
+// this.makeActiveSlides(nextBatch);
